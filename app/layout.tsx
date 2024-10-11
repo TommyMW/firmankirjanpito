e6a3c06f4066d3c4096a53c9c09418e560e63996
TommyMW <tuomas.mustonen05@proton.me>
2024-10-11 09:12:08 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }:
   return (
     <html lang="en">
       <head>
-        <title>Draive - AI Drive-Thrus</title>
+        <title>Tee-se-itse-kirjanpito</title>
       </head>
       <body>
         <div className="">
